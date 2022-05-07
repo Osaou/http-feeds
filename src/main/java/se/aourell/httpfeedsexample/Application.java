5367b6c80f9f0fc0937a596d85581c4ebc27dedf
Osaou <osaourell@gmail.com>
2022-05-08 01:29:28 +0200

    simplify example code

--- src/main/java/se/aourell/httpfeedsexample/Application.java
+++ src/main/java/se/aourell/httpfeedsexample/Application.java
@@ -2,8 +2,10 @@ package se.aourell.httpfeedsexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"se.aourell.httpfeeds", "se.aourell.httpfeedsexample"})
+@EnableScheduling
 public class Application {
 
   public static void main(String[] args) {
