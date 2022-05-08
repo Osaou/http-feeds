44e829f6e6306e6327c8b6367e9ce29eb535de4d
Osaou <osaourell@gmail.com>
2022-05-08 03:30:18 +0200

    ensure time is correctly serialized in json response

--- src/main/java/se/aourell/httpfeeds/core/CloudEvent.java
+++ src/main/java/se/aourell/httpfeeds/core/CloudEvent.java
@@ -1,11 +1,18 @@
 package se.aourell.httpfeeds.core;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import se.aourell.httpfeeds.spi.EventSerializer;
 
+import java.io.IOException;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
-public record CloudEvent(String specversion, String id, String type, OffsetDateTime time, String subject, String method, String datacontenttype, Object data) {
+public record CloudEvent(String specversion, String id, String type, @JsonSerialize(using = OffsetDateTimeSerializer.class) OffsetDateTime time, String subject, String method, String datacontenttype, Object data) {
 
   public static class Mapper {
 
@@ -26,6 +33,21 @@ public record CloudEvent(String specversion, String id, String type, OffsetDateT
         feedItem.method() != null ? null : "application/json",
         feedItem.method() != null ? null : eventSerializer.toEvent(feedItem.data())
       );
+    }
+  }
+
+  public static class OffsetDateTimeSerializer extends JsonSerializer<OffsetDateTime> {
+
+    private static final DateTimeFormatter ISO_8601_FORMATTER = DateTimeFormatter
+      .ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
+      .withZone(ZoneId.of("UTC"));
+
+    @Override
+    public void serialize(OffsetDateTime value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+      if (value == null) {
+        throw new IllegalArgumentException("OffsetDateTime argument is null");
+      }
+      jsonGenerator.writeString(ISO_8601_FORMATTER.format(value));
     }
   }
 }
--- src/main/java/se/aourell/httpfeeds/infrastructure/spring/HttpFeedsController.java
+++ src/main/java/se/aourell/httpfeeds/infrastructure/spring/HttpFeedsController.java
@@ -3,7 +3,6 @@ package se.aourell.httpfeeds.infrastructure.spring;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import se.aourell.httpfeeds.core.CloudEvent;
